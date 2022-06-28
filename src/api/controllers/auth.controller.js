506dc2c20df52954ef75da39f7f0ed43872babb8
rasto <rasto.kopal@gmail.com>
2022-06-28 22:52:50 +0200

    init project

---
+++ src/api/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+const httpStatus = require('http-status');
+const moment = require('moment-timezone');
+const { omit } = require('lodash');
+const User = require('../models/user.model');
+const RefreshToken = require('../models/refreshToken.model');
+const { jwtExpirationInterval } = require('../../config/vars');
+
+/**
+ * Returns a formated object with tokens
+ * @private
+ */
+function generateTokenResponse(user, accessToken) {
+  const tokenType = 'Bearer';
+  const refreshToken = RefreshToken.generate(user).token;
+  const expiresIn = moment().add(jwtExpirationInterval, 'minutes');
+  return {
+    tokenType,
+    accessToken,
+    refreshToken,
+    expiresIn,
+  };
+}
+
+/**
+ * Returns jwt token if registration was successful
+ * @public
+ */
+exports.register = async (req, res, next) => {
+  try {
+    const userData = omit(req.body, 'role');
+    const user = await new User(userData).save();
+    const userTransformed = user.transform();
+    const token = generateTokenResponse(user, user.token());
+    res.status(httpStatus.CREATED);
+    return res.json({ token, user: userTransformed });
+  } catch (error) {
+    return next(User.checkDuplicateEmail(error));
+  }
+};
+
+/**
+ * Returns jwt token if valid username and password is provided
+ * @public
+ */
+exports.login = async (req, res, next) => {
+  try {
+    const { user, accessToken } = await User.findAndGenerateToken(req.body);
+    const token = generateTokenResponse(user, accessToken);
+    const userTransformed = user.transform();
+    return res.json({ token, user: userTransformed });
+  } catch (error) {
+    return next(error);
+  }
+};
---
+++ src/api/validations/auth.validation.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+
+module.exports = {
+  // POST /v1/auth/register
+  register: {
+    body: {
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string()
+        .required()
+        .min(6)
+        .max(128),
+    },
+  },
+
+  // POST /v1/auth/login
+  login: {
+    body: {
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string()
+        .required()
+        .max(128),
+    },
+  },
+
+};
